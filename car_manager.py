ad98a42105b20a4a66f7bc1425dbfb83f4390dc3
Cosmin817 <cosmincmc@gmail.com>
2023-09-04 19:23:39 +0300

    cch update

--- car_manager.py
+++ car_manager.py
@@ -1,7 +1,25 @@
+from turtle import Turtle
+from random import randint
 COLORS = ["red", "orange", "yellow", "green", "blue", "purple"]
 STARTING_MOVE_DISTANCE = 5
 MOVE_INCREMENT = 10
 
 
-class CarManager:
-    pass
+class CarManager(Turtle):
+    def __init__(self):
+        super().__init__()
+        self.init_car()
+
+    def init_car(self):
+        initial_x = 320
+        initial_y = randint(-250, 250)
+        self.color(COLORS[randint(0, len(COLORS) - 1)])
+        self.penup()
+        self.shape("square")
+        self.shapesize(stretch_wid=1, stretch_len=2, outline=None)
+        self.speed("fastest")
+        self.setheading(180)
+        self.goto(initial_x, initial_y)
+
+    def move_car(self):
+        self.forward(STARTING_MOVE_DISTANCE)
--- main.py
+++ main.py
@@ -1,14 +1,31 @@
 import time
 from turtle import Screen
 from player import Player
-from car_manager import CarManager
+from car_manager import CarManager, STARTING_MOVE_DISTANCE, MOVE_INCREMENT
 from scoreboard import Scoreboard
 
 screen = Screen()
 screen.setup(width=600, height=600)
 screen.tracer(0)
 
+player = Player()
+cars = []
+screen.listen()
+screen.onkeypress(fun=player.move_up, key="Up")
+
 game_is_on = True
+counter = 0
+
 while game_is_on:
+    counter += 1
+    for car in cars:
+        car.move_car()
+        if car.xcor() < -350:
+            del car
+    if counter == 6:
+        counter = 0
+        cars.append(CarManager())
     time.sleep(0.1)
     screen.update()
+
+
