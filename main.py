8c86da1e1d9b386abe9cc326ece3008f5e332e0b
Cosmin817 <cosmincmc@gmail.com>
2023-09-18 19:20:49 +0300

    CCH_V2

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-
+from tkinter import *
+import time
 # ---------------------------- CONSTANTS ------------------------------- #
 PINK = "#e2979c"
 RED = "#e7305b"
@@ -9,10 +10,51 @@ WORK_MIN = 25
 SHORT_BREAK_MIN = 5
 LONG_BREAK_MIN = 20
 
+CHECK_MARK = "✔"
+
 # ---------------------------- TIMER RESET ------------------------------- # 
 
-# ---------------------------- TIMER MECHANISM ------------------------------- # 
+# ---------------------------- TIMER MECHANISM ------------------------------- #
 
+def start_timer:
+    count_down(5)
+    
 # ---------------------------- COUNTDOWN MECHANISM ------------------------------- # 
+def count_down(count):
+    canvas.itemconfig(timer_text, text=count)
+    if count > 0:
+        window.after(1000, count_down, count - 1)
 
 # ---------------------------- UI SETUP ------------------------------- #
+window = Tk()
+window.title("Pomodoro")
+window.config(padx=100, pady=50, bg=YELLOW)
+
+
+# Timer LABEL
+timer_label = Label(text="Timer", bg=YELLOW, fg=GREEN, font=(FONT_NAME, 35, "bold"))
+timer_label.grid(column=1, row=0)
+
+# Canvas
+canvas = Canvas(width=200, height=224, bg=YELLOW, highlightthickness=0)
+tomato_img = PhotoImage(file='tomato.png')
+canvas.create_image(103, 112, image=tomato_img)
+timer_text = canvas.create_text(103, 130, text="00:00", fill="white", font=(FONT_NAME, 35, "bold"))
+canvas.grid(row=1, column=1)
+
+
+
+# Start BUTTON
+start_button = Button(text="Start", bg='white')
+start_button.grid(column=0, row=2)
+
+
+# Reset BUTTON
+reset_button = Button(text="Reset", bg='white')
+reset_button.grid(column=2, row=2)
+
+# CheckMark LABER
+check_mark_label = Label(text=CHECK_MARK, bg=YELLOW, fg=GREEN, font=(FONT_NAME, 20, "bold"))
+check_mark_label.grid(column=1, row=3)
+
+window.mainloop()
