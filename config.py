b0ca65ab45e2c61004332e5cec5349dc6a3d1547
Cosmin817 <cosmincmc@gmail.com>
2023-10-24 14:52:51 +0300

    final projecy

---
+++ config.py
@@ -0,0 +1,4 @@
+my_email = 'cch391.test.email@gmail.com'
+password = 'jklpuyrkoogfrwcx'
+string_message_to_send = "Hello,\n\nISS is overhead !\nLook up in the sky.\n\nThanks,\nCosmin"
+person = 'cch392.test@yahoo.com'
--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 import requests
 from datetime import datetime
+import smtplib
+from config import *
+import time
 
-MY_LAT = 51.507351 # Your latitude
-MY_LONG = -0.127758 # Your longitude
+MY_LAT = 44.426765  # Your latitude
+MY_LONG = 26.102537  # Your longitude
 
 response = requests.get(url="http://api.open-notify.org/iss-now.json")
 response.raise_for_status()
@@ -10,6 +13,7 @@ data = response.json()
 
 iss_latitude = float(data["iss_position"]["latitude"])
 iss_longitude = float(data["iss_position"]["longitude"])
+
 
 #Your position is within +5 or -5 degrees of the ISS position.
 
@@ -27,10 +31,28 @@ sunrise = int(data["results"]["sunrise"].split("T")[1].split(":")[0])
 sunset = int(data["results"]["sunset"].split("T")[1].split(":")[0])
 
 time_now = datetime.now()
+time_now_hour = int(time_now.strftime('%H'))
 
-#If the ISS is close to my current position
-# and it is currently dark
-# Then send me an email to tell me to look up.
+
+# If the ISS is close to my current position and it is currently dark
+# if (MY_LAT - 5 <= iss_latitude <= MY_LAT + 5) and (sunrise <= time_now_hour <= sunset):
+
+while True:
+    if (MY_LAT - 5 <= MY_LAT <= MY_LAT + 5) and (sunrise <= time_now_hour <= sunset):
+        # Then send me an email to tell me to look up.
+        with smtplib.SMTP("smtp.gmail.com:587") as connection:
+            connection.starttls()
+            connection.login(user=my_email, password=password)
+            connection.sendmail(
+                from_addr=my_email,
+                to_addrs=person,
+                msg=f"Subject:ISS Alert !\n\n{string_message_to_send}."
+            )
+        print('I just sent an email using Python smtplib')
+        print(f'{iss_latitude} {iss_longitude}')
+    time.sleep(60)
+
+
 # BONUS: run the code every 60 seconds.
 
 
