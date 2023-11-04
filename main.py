1942231afa64306291d6303e5b0902e113fc77ee
Cosmin817 <cosmincmc@gmail.com>
2023-11-04 17:12:55 +0200

    final project

--- main.py
+++ main.py
@@ -3,15 +3,72 @@
 # https://pixe.la/
 
 import requests
+from datetime import datetime
 
-pixela_endpoint = "https://pixe.la/v1/users"
-user_params = {
-    "token": "mamaliga_goala_haha",
-    "username": "zed",
-    "agreeTermsOfService": "yes",
-    "notMinor": "yes"
-}
+# USERNAME = "zed"
+# TOKEN = "mamaliga_goala_haha"
+
+# pixela_endpoint = "https://pixe.la/v1/users"
+# user_params = {
+#     "token": TOKEN,
+#     "username": USERNAME,
+#     "agreeTermsOfService": "yes",
+#     "notMinor": "yes"
+# }
 
 # response = requests.post(url=f"{pixela_endpoint}", json=user_params)
 # print(response.text)
+
+# graph_endpoint = f"{pixela_endpoint}/{USERNAME}/graphs"
+
+# graph_config = {
+#     "id": "graph1",
+#     "name": "Cycling Graph",
+#     "unit": "Km",
+#     "type": "float",
+#     "color": "ajisai"
+# }
+#
+# headers = {
+#     "X-USER-TOKEN": TOKEN
+# }
+
+# response = requests.post(url=graph_endpoint, json=graph_config, headers=headers)
+# print(response.text)
+
+# graph_endpoint = f"{pixela_endpoint}/{USERNAME}/graphs/graph1"
+#
+# today = datetime.now()
+# print(today)
+# print(today.strftime("%Y%m%d"))
+
+# pixel_config = {
+#     "date": today.strftime("%Y%m%d"),
+#     "quantity": "100"
+#  }
+
+# headers = {
+#      "X-USER-TOKEN": TOKEN
+#  }
+#
+# response = requests.post(url=graph_endpoint, json=pixel_config, headers=headers)
+
+USERNAME = "zed"
+GRAPHID = "graph1"
+today = datetime.now()
+update_string = today.strftime("%Y%m%d")
+endpoint = f"https://pixe.la/v1/users/{USERNAME}/graphs/{GRAPHID}/{update_string}"
+TOKEN = "mamaliga_goala_haha"
+
+headers = {
+      "X-USER-TOKEN": TOKEN
+}
+
+pixel_config = {
+    "quantity": "400"
+ }
+
+response = requests.put(url=endpoint, json=pixel_config, headers=headers)
+print(response)
+print(update_string)
 
